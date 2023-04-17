8f57ecf3e410aa5ff6ec2081f1b09ec6ee274f79
Chasechimeric <ryfong704@gmail.com>
2023-04-16 21:12:04 -0700

    made header file, updated main file with test code created test deck file (cardDeck.dat) created functions: newCardNode insertAfter countHand getFromIndex initDeck listHand removeFrom shuffleHand

--- main.c
+++ main.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "cardNode.h"
+#define DECK_FILE_PATH "./cardDeck.dat"
 
 int main(){
-    printf("hey");
+    card *deckHead = NULL, *playerHand = NULL, *player2Hand = NULL, *shuffleHead = NULL;
+    deckHead = (card*)malloc(sizeof(card));
+    newCardNode(deckHead, NULL, -1, 'C', 'R'); 
+    initDeck(deckHead, DECK_FILE_PATH);
+    shuffleHead = (card*)malloc(sizeof(card));
+    playerHand = (card*)malloc(sizeof(card));
+    player2Hand = (card*)malloc(sizeof(card));
+    newCardNode(deckHead, NULL, -1, '#', '#'); 
+    newCardNode(playerHand, NULL, -2, '#', '#');
+    newCardNode(player2Hand, NULL, -3, '#', '#');
+    
+    listHand(deckHead);
+    printf("\n");
+    card *currentNode = (deckHead->nextCard);
+    card* testFind = getFromIndex(deckHead, 51);
+    shuffleHand(deckHead, shuffleHead);
+    shuffleHand(shuffleHead, deckHead);
+    shuffleHand(deckHead, shuffleHead);
+    shuffleHand(shuffleHead, deckHead);
+    shuffleHand(deckHead, shuffleHead);
+    shuffleHand(shuffleHead, deckHead);
+    shuffleHand(deckHead, shuffleHead);
+    shuffleHand(shuffleHead, deckHead);
+
+    listHand(deckHead);
+    int temp = countHand(deckHead);
+
+
+    // test print deck
+
     return 0;
 }
